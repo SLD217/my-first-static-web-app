ab41ced6faf8959c0881e006e5495ac27452619a
SLD217 <neal.mcconnell@gmail.com>
2024-09-01 09:28:01 +0100

    Changes to be committed: 	deleted:    src/App1.js 	modified:   src/index.js

--- src/App1.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function App2() {
-  const value = 'World this test is this the case';
-  return <div>Hello {value}</div>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import App2 from './App1';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<App2 />, document.getElementById('root'));
